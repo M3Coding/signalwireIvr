00996ba6e56c131d8f5ccd16f16621b31b0355a6
M3Coding <meady2009@gmail.com>
2025-08-02 19:41:20 -0400

    comments added

--- index.js
+++ index.js
@@ -13,24 +13,31 @@ const client = await SignalWire({
     context: process.env.CONTEXT,
     supportNumber: process.env.SUPPORT_NUMBER,
     timeZone: process.env.TZ,
-});
-const voiceClient = client.voice;
+});// Signalwire client account information
+const voiceClient = client.voice; //sets up the voice client method
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms)); //delays time before the automated voice begins talking. 
 
+//Promise is used to wait for call to pickup and then begin automation. 
 await voiceClient.listen({
-    topics: ["ivr"],
+    topics: ["ivr"],//this is the name of the project topic in signalwire
     onCallReceived: async (call) => {
-        console.log("Incoming call from:", call.from);
+        await call.answer();//wait for call to be picked up
+        console.log("Incoming call from:", call.from);//displays who is calling from the JSON information.
         
-        await call.answer();
+        
+        await sleep(7000);
         await call.playTTS({
             text: "Thank you for call Coding 4 Life Corp! Listen for the following menu options."
-        });
+        });//play text to speech information
+        call.hangup(); //hangs up the call
     }
 });
 console.log("Waiting for calls...");
-
+//displays waiting for call in console when node is ran 
 
 app.listen(3000, () => {
     console.log("Server running on port 3000.")
     
 });
+
+// real world application use needs ngrok 
