d7c3a3853629ffee45393a175d17a1bcc0ac87ca
M3Coding <meady2009@gmail.com>
2025-08-03 03:49:14 -0400

    add back to main menu logic for case 1 and 2

--- index.js
+++ index.js
@@ -29,8 +29,9 @@ await voiceClient.listen({
         /* await call.playTTS({
             text: "Thank you for call Coding 4 Life Corp! Listen for the following menu options."
         });//play text to speech information */
-        const prompts = await call.promptTTS ({
-            text: `<speak>Thank you for calling Coding 4 Life Corp. Please listen to the following menu options: For general information press 1, for hours press 2, and for support press 3. </speak>`,
+        async function mainMenu(call) {
+            const prompts = await call.promptTTS ({
+            text: `<speak>Thank you for calling Coding 4 Life Corp., Please listen to the following menu options: For general information press 1, for hours press 2, and for support press 3. </speak>`,
             digits: {
                 max: 1,
                 digitTimeout: 10,
@@ -48,24 +49,26 @@ await voiceClient.listen({
                await call.playTTS({
                 text: `<speak>Start with us today by emailing your request to coding life at g mail dot com or visiting our website at www coding 4 life corp dot com and completing our request form.
 
-Coding 4 Life Corp is a technology-driven organization focused on creating innovative digital solutions that empower businesses and communities. We offer services in software development, IT consulting, automation, and education. Whether you're building from the ground up or improving existing systems, we're here to help with all your development needs.
+Coding 4 Life Corp is a technology-driven organization focused on creating innovative digital solutions that empower businesses and communities. We offer services in software development, IT consulting, automation, and education. Whether you're building from the ground up or improving existing systems, we are here to help with all your development needs.
 
 Founded and led by Matthew Eady, an experienced IT professional and passionate developer, Coding 4 Life Corp is built on a commitment to excellence, integrity, and innovation. Under Matthews leadership, we aim to bridge the gap between technology and everyday life by delivering user-friendly, scalable, and impactful solutions.
 
-At Coding 4 Life Corp, we believe in coding with purpose.</speak>`
+At Coding 4 Life Corp, we believe in coding with purpose. Back to the main menu!</speak>`
                })
-               await call.hangup();
+               await sleep(2000);
+               await mainMenu(call); //return main menu
                break;
             case '2':
                 await call.playTTS({
-                text: `<speak>Thank you for contacting us. Our service hours are from 8:00 AM to 9:00 PM Eastern Time. If you're reaching out outside of these hours, we'll get back to you as soon as we're available. We appreciate your patience!</speak>`
+                text: `<speak>Thank you for contacting us. Our service hours are from 8:00 AM to 9:00 PM Eastern Time. If you are reaching out outside of these hours, we will get back to you as soon as we're available. We appreciate your patience! Back to the main menu!</speak>`
                })
-               await call.hangup();
+               await sleep(2000);
+               await mainMenu(call);
                break;
                
             case '3':
                 await call.connectPhone({
-                    to: SUPPORT_NUMBER,
+                    to: supportNumnber,
                 })
                 
                 break;
@@ -75,9 +78,11 @@ At Coding 4 Life Corp, we believe in coding with purpose.</speak>`
                 break;
 
         }
-        
+        } //end of the prompts function
+        await mainMenu(call); //calls the main menu function to start the IVR system
     }
-});
+})
+
 console.log("Waiting for calls...");
 //displays waiting for call in console when node is ran 
 
